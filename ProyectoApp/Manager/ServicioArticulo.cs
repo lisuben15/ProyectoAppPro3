8cdd5ff0a99141953b0975e03dbae5fa10b80d22
Ariel Damián Rodríguez <ariel.fonavi17@gmail.com>
2023-09-09 12:59:27 -0300

    Se agregó clase manager y dentro, prototipo de funcion.

---
+++ ProyectoApp/Manager/ServicioArticulo.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoApp.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoApp.Manager
+{
+    public class ServicioArticulo
+    {
+        public void AgregarArticulo(Articulo articulo)
+        {
+
+        }
+        public void ModificarArticulo(Articulo articulo)
+        {
+
+        }
+        public void EliminarArticulo(Articulo articulo)
+        {
+
+        }
+        public List<string> ListarArticulos()
+        {
+            return new List<string>();
+        }
+    }  
+}
