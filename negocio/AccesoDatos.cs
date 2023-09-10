4a11554e8e0e2558bdda95ed3ccdfa2e2a26db6b
lisuben15 <garcia_gaston_1991@hotmail.com>
2023-09-10 14:06:38 -0300

    se creó parte de la clase AccesoDatos

---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("va la cadena de conexion");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura() // realiza la lectura y guarda en lector
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null) 
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
